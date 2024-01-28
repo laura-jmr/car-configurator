892bbe7a9428a40d8edfd427e4215d557e389fe2
laura-jmr <laura.juergensmeier@gmail.com>
2024-01-28 16:11:54 +0100

    feat: calculate totals

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -24,6 +24,40 @@ export default function Landing() {
         setSelectedPS(ps);
     };
 
+    const calculatePrice = () => {
+        let basePrice = 30000;
+        let colorPrice = 0;
+        let psPrice = 0;
+
+        switch (selectedColor) {
+            case 'Blau':
+                colorPrice = 0;
+                break;
+            case 'Gelb':
+                colorPrice = 500;
+                break;
+            case 'Weiß':
+                colorPrice = 200;
+                break;
+            default:
+                colorPrice = 0;
+        }
+
+        switch (selectedPS) {
+            case '280 PS':
+                psPrice = 0;
+                break;
+            case '350 PS':
+                psPrice = 3000;
+                break;
+            default:
+                psPrice = 0;
+        }
+
+        let totalPrice = basePrice + colorPrice + psPrice;
+        return totalPrice;
+    };
+
     console.log("selected color value: " + selectedColor);
     return (
         <div id='landing-page-container'>
@@ -35,6 +69,9 @@ export default function Landing() {
                 <div>
                     <Dropdown title="Lackierung" options={["Blau", "Gelb", "Weiss"]} onSelect={handleColorChange} />
                     <Dropdown title="Motorleistung" options={["280 PS", "350 PS"]} onSelect={handlePSChange} />
+                </div>
+                <div id="total-price">
+                    <p>Gesamtpreis: {calculatePrice()} €</p>
                 </div>
             </div>
             <CarRender scale={8} position={[0, -3, 0]} color={selectedColor} ps={selectedPS}/>
